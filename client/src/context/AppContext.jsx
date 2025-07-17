f86e969d30857d20f5adf7fc8d244120eba7ab3a
Ian Wafula <neocorpse@proton.me>
2025-07-17 22:23:54 +0300

    Create routes, context and boilerplate files

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+function AppProvider({children}) {
+    return <AppContext.Provider>{children}</AppContext.Provider>
+}
+
+export {AppContext, AppProvider}
