6a39c2f37295a48d78da796361527cfddf0bfdd8
Ian Wafula <neocorpse@proton.me>
2025-07-18 11:42:05 +0300

    Add sign up form
    Included validation and api fetch

--- client/vite.config.js
+++ client/vite.config.js
@@ -26,5 +26,12 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		cors: true,
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:5555/',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
 	},
 });
