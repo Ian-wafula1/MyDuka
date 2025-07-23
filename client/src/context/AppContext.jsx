5c63fe08365583f58950339e3ce1ba180c8b5cec
Ian Wafula <neocorpse@proton.me>
2025-07-23 21:04:44 +0300

    Add Default user to context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,36 @@
-import { createContext } from "react";
+import { createContext } from 'react';
+import { useState } from 'react';
 
 const AppContext = createContext();
 
-function AppProvider({children}) {
-    return <AppContext.Provider>{children}</AppContext.Provider>
+const merchantObj = {
+	id: 1,
+	name: 'Ian',
+	email: 'lZL0D@example.com',
+	password: 'password',
+	account_type: 'merchant',
+	stores: [
+		{
+			id: 12,
+			name: 'Kijiji',
+			location: '123 Main St',
+		},
+		{
+			id: 13,
+			name: 'Amazon',
+			location: '123 Side bara',
+		},
+		{
+			id: 14,
+			name: 'Ebay',
+			location: '123 Side bara',
+		},
+	],
+};
+
+function AppProvider({ children }) {
+    const [currentUser, setCurrentUser] = useState(merchantObj);
+	return <AppContext.Provider value={{ currentUser, setCurrentUser }}>{children}</AppContext.Provider>;
 }
 
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
