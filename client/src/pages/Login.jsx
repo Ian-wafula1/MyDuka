e601f6b0d3020f75db74776e9c5f298fd114d5e4
Ian Wafula <neocorpse@proton.me>
2025-07-24 11:25:32 +0300

    Change validation method for login form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,55 +1,69 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form } from 'formik';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import pic1 from '../assets/pic1.svg';
+import pic2 from '../assets/pic2.svg';
+import * as Yup from 'yup';
+import { MyTextInput, MySelect } from '../utils/formElements';
+import { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
-const Login = ({ onLogin }) => {
-  return (
-    <div>
-    
-      <h1>Login to MyDuka</h1>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = 'Email is required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
+const Login = () => {
+	const navigate = useNavigate();
+	const { setCurrentUser } = useContext(AppContext);
+	const [error, setError] = useState('User does not exist');
+	return (
+		<div className="form-container">
+			<h1>Login to MyDuka</h1>
+			<Formik
+				initialValues={{ email: '', password: '', account_type: '' }}
+				validationSchema={Yup.object({
+					email: Yup.string().email('Invalid email address').required('Required'),
+					password: Yup.string().min(8, 'Must be 8 characters or more').required('Required'),
+					account_type: Yup.string().oneOf(['merchant', 'admin', 'clerk']).required('Required'),
+				})}
+				onSubmit={(values, { setSubmitting }) => {
+					axios
+						.post('/api/login', {
+							email: values.email,
+							password: values.password,
+							account_type: values.account_type,
+						})
+						.then((res) => {
+							localStorage.setItem('token', res.data.token);
+							setCurrentUser(res.data.user_dict);
+							navigate('/');
+						})
+						.catch((err) => {
+							console.log(err);
+							setError(err.response.data.error);
+						});
+					setSubmitting(false);
+				}}>
+				{({ isSubmitting }) => (
+					<Form>
+						<div>
+							<MyTextInput label="Email" name="email" type="email" />
+							<MyTextInput label="Password" name="password" type="password" />
+							<MySelect label="Account Type" name="account_type">
+								<option value="merchant">Merchant</option>
+								<option value="admin">Admin</option>
+								<option value="clerk">Clerk</option>
+							</MySelect>
+						</div>
 
-          if (!values.password) {
-            errors.password = 'Password is required';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          onLogin(values); 
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <div>
-              <label>Email:</label>
-              <Field type="email" name="email" />
-              <ErrorMessage name="email" component="div" />
-            </div>
+						{error && <div className="fetch error">{error}</div>}
 
-            <div>
-              <label>Password:</label>
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
-            </div>
-
-            <button type="submit" disabled={isSubmitting}>
-              Login
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  );
+						<button type="submit" disabled={isSubmitting}>
+							Login
+						</button>
+					</Form>
+				)}
+			</Formik>
+			<img src={pic1} alt="Logo" />
+			<img src={pic2} alt="Logo" />
+		</div>
+	);
 };
 
 export default Login;
