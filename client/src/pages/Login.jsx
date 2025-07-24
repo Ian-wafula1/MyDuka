700f59549d0d1db5dde1e62c5c6f594784a31984
Ian Wafula <neocorpse@proton.me>
2025-07-24 10:47:09 +0300

    Clean up code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,125 +1,55 @@
-// import React from 'react';
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-// const Login = ({ onLogin }) => {
-//   return (
-//     <div>import React from 'react';
-//     import { Formik, Form, Field, ErrorMessage } from 'formik';
-    
-//     const Login = ({ onLogin }) => {
-//       return (
-//         <div>
-//           <h1>Login to MyDuka</h1>
-//           <Formik
-//       <h1>Login to MyDuka</h1>
-//       <Formik
-//         initialValues={{ email: '', password: '' }}
-//         validate={(values) => {
-//           const errors = {};
-//           if (!values.email) {
-//             errors.email = 'Email is required';
-//           } else if (
-//             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-//           ) {
-//             errors.email = 'Invalid email address';
-//           }
-
-//           if (!values.password) {
-//             errors.password = 'Password is required';
-//           }
-//           return errors;
-//         }}
-//         onSubmit={(values, { setSubmitting }) => {
-//           onLogin(values); 
-//           setSubmitting(false);
-//         }}
-//       >
-//         {({ isSubmitting }) => (
-//           <Form>
-//             <div>
-//               <label>Email:</label>
-//               <Field type="email" name="email" />
-//               <ErrorMessage name="email" component="div" />
-//             </div>
-//             import React from 'react';
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
-
-// const Login = ({ onLogin }) => {
-//   return (
-//     <div>import React from 'react';
-//     import { Formik, Form, Field, ErrorMessage } from 'formik';
-    
-//     const Login = ({ onLogin }) => {
-//       return (
-//         <div>
-//           <h1>Login to MyDuka</h1>
-//           <Formik
-//       <h1>Login to MyDuka</h1>
-//       <Formik
-//         initialValues={{ email: '', password: '' }}
-//         validate={(values) => {
-//           const errors = {};
-//           if (!values.email) {
-//             errors.email = 'Email is required';
-//           } else if (
-//             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-//           ) {
-//             errors.email = 'Invalid email address';
-//           }
-
-//           if (!values.password) {
-//             errors.password = 'Password is required';
-//           }
-//           return errors;
-//         }}
-//         onSubmit={(values, { setSubmitting }) => {
-//           onLogin(values); 
-//           setSubmitting(false);
-//         }}
-//       >
-//         {({ isSubmitting }) => (
-//           <Form>
-//             <div>
-//               <label>Email:</label>
-//               <Field type="email" name="email" />
-//               <ErrorMessage name="email" component="div" />
-//             </div>
-
-//             <div>
-//               <label>Password:</label>
-//               <Field type="password" name="password" />
-//               <ErrorMessage name="password" component="div" />
-//             </div>
-
-//             <button type="submit" disabled={isSubmitting}>
-//               Login
-//             </button>
-//           </Form>
-//         )}
-//       </Formik>
-//     </div>
-//             <div>
-//               <label>Password:</label>
-//               <Field type="password" name="password" />
-//               <ErrorMessage name="password" component="div" />
-//             </div>
-
-//             <button type="submit" disabled={isSubmitting}>
-//               Login
-//             </button>
-//           </Form>
-//         )}
-//       </Formik>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-export default function Login() {
+const Login = ({ onLogin }) => {
   return (
     <div>
+    
       <h1>Login to MyDuka</h1>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Email is required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+
+          if (!values.password) {
+            errors.password = 'Password is required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          onLogin(values); 
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label>Email:</label>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+            </div>
+
+            <div>
+              <label>Password:</label>
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" component="div" />
+            </div>
+
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
+
+export default Login;
