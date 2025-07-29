e92a134ca67f952209097fb100c39a66da8af15d
Ian Wafula <neocorpse@proton.me>
2025-07-29 08:39:43 +0300

    Refactor registration routes to improve error handling and response structure

--- server/apis/registration.py
+++ server/apis/registration.py
@@ -6,128 +6,142 @@ from models import User, Merchant, Store
 import datetime, os
 from flask_mail import Message
 
-reg = Namespace('registration', description='Registration operations', path='/')
+reg = Namespace('registration', description='Registration operations', path='/api')
 
 @reg.route('/login')
 class Login(Resource):
     def post(self):
-        account_type = request.json['account_type']
-        if account_type == 'merchant':
-            merchant = Merchant.query.filter_by(email=request.json['email']).first()
-            if not merchant:
-                return make_response({'error': 'Merchant not found'}, 404)
-            if not merchant.authenticate(request.json['password']):
-                return make_response({'error': 'Invalid password'}, 401)
-            access_token = create_access_token(identity={
-                'id': merchant.id,
-                'account_type': 'merchant'
-            }, expires_delta=datetime.timedelta(days=7))
-            return {'access_token': access_token, 'user_dict': merchant.to_dict()}
+        try: 
+            data = request.json
+            if not data or 'email' not in data or 'password' not in data:
+                return make_response({'error': 'Missing email or password'}, 400)
             
-        elif account_type == 'admin':
-            admin = User.query.filter_by(email=request.json['email'], account_type='admin').first()
-            if not admin:
-                return make_response({'error': 'Admin not found'}, 404)
-            if not admin.authenticate(request.json['password']):
+            account_type = data.get('account_type')
+            
+            if account_type == 'merchant':
+                user = Merchant.query.filter_by(email=request.json['email']).first()
+                
+            elif account_type == 'admin':
+                user = User.query.filter_by(email=request.json['email'], account_type='admin').first()
+            
+            elif account_type == 'clerk':
+                user = User.query.filter_by(email=request.json['email'], account_type='clerk').first()
+            else:
+                return make_response({'error': 'Invalid account type'}, 400)
+            
+            if not user:
+                return make_response({'error': 'User not found'}, 404)
+            if not user.authenticate(request.json['password']):
                 return make_response({'error': 'Invalid password'}, 401)
             access_token = create_access_token(identity={
-                'id': admin.id,
-                'account_type': 'admin'
+                'id': user.id,
+                'account_type': account_type
             }, expires_delta=datetime.timedelta(days=7))
-            return {'access_token': access_token, 'user_dict': admin.to_dict()}
-        
-        elif account_type == 'clerk':
-            clerk = User.query.filter_by(email=request.json['email'], account_type='clerk').first()
-            if not clerk:
-                return make_response({'error': 'Clerk not found'}, 404)
-            if not clerk.authenticate(request.json['password']):
-                return make_response({'error': 'Invalid password'}, 401)
-            access_token = create_access_token(identity={
-                'id': clerk.id,
-                'account_type': 'clerk'
-            }, expires_delta=datetime.timedelta(days=7))
-            return {'access_token': access_token, 'user_dict': clerk.to_dict()}
-        else:
-            return make_response({'error': 'Invalid account type'}, 400)
+            return {'access_token': access_token, 'user_dict': user.to_dict()}
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
 
 @reg.route('/signup')
 class Signup(Resource):
     def post(self):
-        data = request.json
+        try:
+            data = request.json
         
-        if (account_type := data['account_type']) == 'merchant':
-            merchant = Merchant(name=data['name'], email=data['email'])
-            merchant.password_hash = data.get('password')
-            db.session.add(merchant)
-            db.session.commit()
+            if (account_type := data['account_type']) == 'merchant':
+                user = Merchant(name=data['name'], email=data['email'])
+            elif account_type in ('admin', 'clerk'):
+                user = User(name=data['name'], email=data['email'], account_type=account_type)
+            else:
+                return make_response({'error': 'Invalid account type'}, 400)
             
-            return make_response({'message': 'Merchant signed up successfully'}, 201)
-        elif account_type in ('admin', 'clerk'):
-            user = User(name=data['name'], email=data['email'], account_type=account_type)
             user.password_hash = data.get('password')
             db.session.add(user)
             db.session.commit()
-            
-            return make_response({'message': 'User signed up successfully'}, 201)
-        
-        else:
-            return make_response({'error': 'Invalid account type'}, 400)
+            return make_response({'message': f'{account_type.capitalize()} signed up successfully'}, 201)
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
         
 @reg.route('/reset-password')
 class ResetPassword(Resource):
     def post(self):
-        data = request.json
-        if (account_type := data['account_type']) == 'merchant':
-            merchant = Merchant.query.filter_by(email=data['email']).first()
-            if not merchant:
-                return make_response({'error': 'Merchant not found'}, 404)
-            merchant.password_hash = data.get('password')
-            db.session.add(merchant)
-            db.session.commit()
+        try:
+            data = request.json
+            if not data or 'email' not in data or 'password' not in data:
+                return make_response({'error': 'Missing email or password'}, 400)
             
-            return make_response({'message': 'Password reset successfully'}, 200)
-        
-        elif account_type in ('admin', 'clerk'):
-            user = User.query.filter_by(email=data['email'], account_type=account_type).first()
+            if (account_type := data['account_type']) == 'merchant':
+                user = Merchant.query.filter_by(email=data['email']).first()
+            elif account_type in ('admin', 'clerk'):
+                user = User.query.filter_by(email=data['email'], account_type=account_type).first()
+            else:
+                return make_response({'error': 'Invalid account type'}, 400)
+                
             if not user:
-                return make_response({'error': 'User not found'}, 404)
+                return make_response({'error': 'Merchant not found'}, 404)
             user.password_hash = data.get('password')
             db.session.add(user)
             db.session.commit()
             
             return make_response({'message': 'Password reset successfully'}, 200)
-        else:
-            return make_response({'error': 'Invalid account type'}, 400)
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
+        
 
 @reg.route('/verify-token')
 class VerifyToken(Resource):
     @jwt_required()
     def get(self):
-        data = get_jwt_identity()
-        return make_response({'email': data['email'],
-                              'store_id': data.get('store_id'),
-                              'store_name': data.get('store_name')},
-                            200)
+        try:
+            data = get_jwt_identity()
+            return make_response({'email': data['email'],
+                                'store_id': data.get('store_id'),
+                                'store_name': data.get('store_name')},
+                                200)
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
         
 @reg.route('/send-invite')
 class SendInvite(Resource):
     @jwt_required()
     def post(self):
-        data = request.json
-        store = Store.query.filter_by(id=data['store_id']).first()
-        if not store:
-            return make_response({'error': 'Store not found'}, 404)
-        access_token = create_access_token(identity={
-            'email': data['email'],
-            'store_id': data['store_id'],
-            'store_name': store.name
-        })
-        message = Message(
-            subject="Invitation to join a store",
-            sender="no-reply@myduka.com",
-            recipients=[data['email']],
-            body=f"Hello,\n\nYou have been invited to join {store.name}. Please click the link below to join:\n{os.environ.get('FRONTEND_URL')}/signup/{access_token}\n\nBest regards,\nMyDuka Team"
-        )
-        
-        mail.send(message)
-        return make_response({'message': 'Invitation sent successfully'}, 200)
+        try:
+            data = request.json
+            store = Store.query.filter_by(id=data['store_id']).first()
+            if not store:
+                return make_response({'error': 'Store not found'}, 404)
+            access_token = create_access_token(identity={
+                'email': data['email'],
+                'store_id': data['store_id'],
+                'store_name': store.name
+            }, expires_delta=datetime.timedelta(hours=2))
+            message = Message(
+                subject="Invitation to join a store",
+                sender="no-reply@myduka.com",
+                recipients=[data['email']],
+                body=f"Hello,\n\nYou have been invited to join {store.name}. Please click the link below to join:\n{os.environ.get('FRONTEND_URL')}/signup/{access_token}\n\nBest regards,\nMyDuka Team"
+            )
+            
+            mail.send(message)
+            return make_response({'message': 'Invitation sent successfully'}, 200)
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
+    
+@reg.route('/me')
+class Me(Resource):
+    @jwt_required()
+    def get(self):
+        try:
+            data = get_jwt_identity()
+            if (account_type := data['account_type']) == 'merchant':
+                user = Merchant.query.filter_by(id=data['id']).first()
+            elif account_type in ('admin', 'clerk'):
+                user = User.query.filter_by(id=data['id']).first()
+            else:
+                return make_response({'error': 'Invalid account type'}, 400)
+            
+            make_response({
+                'account_type': account_type, 'user_dict': user.to_dict()}
+                        , 200)
+            
+        except Exception as e:
+            return make_response({'error': str(e)}, 500)
